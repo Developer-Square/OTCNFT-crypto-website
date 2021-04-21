034b90c00b7089cb8e43c05a31f9243ac40587f9
Ryan Njoroge <keddelyronjoz@gmail.com>
2021-04-21 23:03:34 +0300

    Added token selection functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,59 @@
+let value;
+
+
+// Read the json file with the token list and display it,
+// onclick of the button
+const loadTokens = () => {
+    let tokenContainer = document.querySelector('#token-container')
+    tokenData.map(token => {
+        if(tokenContainer) {
+            // First create the outer container.
+            let selectOption = document.createElement('div')
+            selectOption.className = 'select-otion'
+
+            // Then create the inner elements.
+            let img = document.createElement('img')
+            img.src = token.imageUrl
+            img.className = 'token-img'
+            selectOption.appendChild(img)
+
+            let span = document.createElement('span')
+            span.className = 'token'
+
+            let tokenTitle = document.createElement('div')
+            tokenTitle.className = 'token-title'
+            tokenTitle.title = token.content
+            // Add the actual title to the div
+            tokenTitle.innerHTML = token.title
+            let tokenSubtitle = document.createElement('div')
+            tokenSubtitle.className = 'token-subtitle'
+            tokenSubtitle.innerHTML = token.content
+
+            // Add the the subtitle and title to the span.
+            span.appendChild(tokenTitle)
+            span.appendChild(tokenSubtitle)
+
+            // Then add the span to the selectOption.
+            selectOption.appendChild(span)
+
+            // And finally to the tokenContainer.
+            tokenContainer.appendChild(selectOption)
+        }
+    })
+}
+
+// Set the chosen token to the page.
+const setTokens = (evt) => {
+    let img = document.querySelector('.from-swap-image')
+    let title = document.querySelector('.from-swap-title')
+    let selectedTitle = evt.currentTarget.innerText.slice(0,3)
+
+    if (img && title) {
+        img.src = `./images/${selectedTitle}.png`
+        title.innerHTML = selectedTitle
+    }
+}
+
 
 // Toggle the tabs on th page through the toggle buttons
 function toggleTabs(evt, cityName) {
---
+++ resources/token.js
@@ -0,0 +1,232 @@
+const tokenData = [
+    {
+        "imageUrl": "./images/download.png",
+        "title": "ETH",
+        "content": "Ether"
+    },
+    {
+        "imageUrl": "./images/1-inch.png",
+        "title": "1INCH",
+        "content": "1INCH"
+    },
+    {
+        "imageUrl": "./images/AAVE.png",
+        "title": "AAVE",
+        "content": "AAVE"
+    },
+    {
+        "imageUrl": "./images/AMP.png",
+        "title": "AMP",
+        "content": "AMP"
+    },
+    {
+        "imageUrl": "./images/ANT.png",
+        "title": "ANT",
+        "content": "Aragon Network Token"
+    },
+    {
+        "imageUrl": "./images/bal.png",
+        "title": "BAL",
+        "content": "Balancer"
+    },
+    {
+        "imageUrl": "./images/band.png",
+        "title": "BAND",
+        "content": "Band Protocol"
+    },
+    {
+        "imageUrl": "./images/bat.png",
+        "title": "BAT",
+        "content": "BacicAttensionToken"
+    },
+    {
+        "imageUrl": "./images/BNT.png",
+        "title": "BNT",
+        "content": "Bancor Network Token"
+    },
+    {
+        "imageUrl": "./images/COMP.png",
+        "title": "COMP",
+        "content": "Compound"
+    },
+    {
+        "imageUrl": "./images/CRV.png",
+        "title": "CRV",
+        "content": "Curve DAO Token"
+    },
+    {
+        "imageUrl": "./images/CVC.png",
+        "title": "CVC",
+        "content": "Civic"
+    },
+    {
+        "imageUrl": "./images/DAI.png",
+        "title": "DAI",
+        "content": "Dai Stablecoin"
+    },
+    {
+        "imageUrl": "./images/DNT.png",
+        "title": "DNT",
+        "content": "district0x"
+    },
+    {
+        "imageUrl": "./images/ENJ.png",
+        "title": "ENJ",
+        "content": "Enjin"
+    },
+    {
+        "imageUrl": "./images/GNO.png",
+        "title": "GNO",
+        "content": "Gnosis Token"
+    },
+    {
+        "imageUrl": "./images/GRT.png",
+        "title": "GRT",
+        "content": "The Graph"
+    },
+    {
+        "imageUrl": "./images/GUSD.png",
+        "title": "GUSD",
+        "content": "GeminiDollar"
+    },
+    {
+        "imageUrl": "./images/KEEP.png",
+        "title": "KEEP",
+        "content": "Keep Network"
+    },
+    {
+        "imageUrl": "./images/KNC.png",
+        "title": "KNC",
+        "content": "Kyber Network Crystal"
+    },
+    {
+        "imageUrl": "./images/LINK.png",
+        "title": "LINK",
+        "content": "ChainLink Token"
+    },
+    {
+        "imageUrl": "./images/LOOM.png",
+        "title": "LOOM",
+        "content": "Loom Network"
+    },
+    {
+        "imageUrl": "./images/LRC.png",
+        "title": "LRC",
+        "content": "LoopringCoin V2"
+    },
+    {
+        "imageUrl": "./images/MANA.png",
+        "title": "MANA",
+        "content": "Decentraland"
+    },
+    {
+        "imageUrl": "./images/MKR.png",
+        "title": "MKR",
+        "content": "Maker"
+    },
+    {
+        "imageUrl": "./images/MLN.png",
+        "title": "MLN",
+        "content": "Melon"
+    },
+    {
+        "imageUrl": "./images/NMR.png",
+        "title": "NMR",
+        "content": "Numeraire"
+    },
+    {
+        "imageUrl": "./images/NU.png",
+        "title": "NU",
+        "content": "NuCypher"
+    },
+    {
+        "imageUrl": "./images/OXT.png",
+        "title": "OXT",
+        "content": "Orchid"
+    },
+    {
+        "imageUrl": "./images/PAXG.png",
+        "title": "PAXG",
+        "content": "PaxosGold"
+    },
+    {
+        "imageUrl": "./images/REN.png",
+        "title": "REN",
+        "content": "Republic Token"
+    },
+    {
+        "imageUrl": "./images/REP.png",
+        "title": "REP",
+        "content": "Reputation Augur v1"
+    },
+    {
+        "imageUrl": "./images/REPV2.png",
+        "title": "REPv2",
+        "content": "Reputation Augur v2"
+    },
+    {
+        "imageUrl": "./images/SAND.png",
+        "title": "SAND",
+        "content": "Sandbox"
+    },
+    {
+        "imageUrl": "./images/SKL.png",
+        "title": "SKL",
+        "content": "Skale"
+    },
+    {
+        "imageUrl": "./images/SNX.png",
+        "title": "SNX",
+        "content": "Synthetix Network Token"
+    },
+    {
+        "imageUrl": "./images/STORJ.png",
+        "title": "STORJ",
+        "content": "Storj Token"
+    },
+    {
+        "imageUrl": "./images/TBTC.png",
+        "title": "TBTC",
+        "content": "tBTC"
+    },
+    {
+        "imageUrl": "./images/UMA.png",
+        "title": "UMA",
+        "content": "UMA Voting Token v1"
+    },
+    {
+        "imageUrl": "./images/UNI.png",
+        "title": "UNI",
+        "content": "Uniswap"
+    },
+    {
+        "imageUrl": "./images/USDC.png",
+        "title": "USDC",
+        "content": "USDCoin"
+    },
+    {
+        "imageUrl": "./images/USDT.png",
+        "title": "USDT",
+        "content": "Tether USD"
+    },
+    {
+        "imageUrl": "./images/WBTC.png",
+        "title": "WBTC",
+        "content": "Wrapped BTC"
+    },
+    {
+        "imageUrl": "./images/WETH.png",
+        "title": "WETH",
+        "content": "Wrapped Ether"
+    },
+    {
+        "imageUrl": "./images/YFI.png",
+        "title": "YFI",
+        "content": "yearn finance"
+    },
+    {
+        "imageUrl": "./images/ZRX.png",
+        "title": "ZRX",
+        "content": "0x Protocol Token"
+    }
+]
