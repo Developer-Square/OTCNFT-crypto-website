13dc69f17f4dc7428a741f9796e2848d28fb916d
Ryan Njoroge <keddelyronjoz@gmail.com>
2021-04-23 13:41:54 +0300

    feat: Added select token functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 let page;
+let buttonClicked;
 
 
 // Read the json file with the token list and display it,
 // onclick of the button
-const loadTokens = () => {
+const loadTokens = (buttonClickedClassName) => {
+  buttonClicked = buttonClickedClassName
+
   let tokenContainer = document.querySelector('#token-container')
     tokenData.map(token => {
         if(tokenContainer) {
@@ -43,6 +46,72 @@ const loadTokens = () => {
     })
 }
 
+// Changes 'select-token' button to a button with actual currency.
+const setSecondTokenButton = (imageUrl, titleText, imgClassName, titleClassName) => {
+  let selectSwapTokenButton = document.querySelector('.select-token-container-swap')
+  let selectPurchaseTokenButton = document.querySelector('.select-token-container-purchase')
+  let swapTokenButton = document.querySelector('.to-swap')
+  let purchaseTokenButton = document.querySelector('.to-purchase')
+  let toSwapInput = document.querySelector('.to-input-swap')
+  let toPurchaseInput = document.querySelector('.to-input')
+  let swapTokenIcon = document.querySelector('.to-swap-image')
+  let swapTokenTitle = document.querySelector('.to-swap-title')
+  let purchaseTokenIcon = document.querySelector('.to-purchase-image')
+  let purchaseTokenTitle = document.querySelector('.to-purchase-title')
+
+  if (buttonClicked.includes('to-swap')) {
+    // Hide the 'select a token' button.
+    selectSwapTokenButton.style.display = 'none'
+  } else if (buttonClicked.includes('to-purchase')) {
+    selectPurchaseTokenButton.style.display = 'none'
+  }
+
+  // Check if element already exists,
+  // If yes then just replace the text.
+  if (swapTokenIcon && swapTokenTitle && buttonClicked.includes('to-swap')) {
+    swapTokenIcon.src = imageUrl
+    swapTokenTitle.innerHTML = titleText
+  } else if (purchaseTokenIcon && purchaseTokenTitle && buttonClicked.includes('to-purchase')) {
+    purchaseTokenIcon.src = imageUrl
+    purchaseTokenTitle.innerHTML = titleText
+  } else {
+
+    // Create the token, same as the one from section.
+    let textContainer = document.createElement('span')
+    let img = document.createElement('img')
+    let title = document.createElement('span')
+    let icon = document.createElement('span')
+    let dropDownIcon = document.createElement('i')
+
+    textContainer.className = 'ETH-span'
+    img.className = imgClassName
+    img.src = imageUrl
+    title.className = `eth-title ${titleClassName}`
+    title.innerHTML = titleText
+    icon.className = 'eth-icon'
+    dropDownIcon.className = 'fa fa-angle-down'
+    dropDownIcon.setAttribute('aria-hidden', 'true')
+
+    // Arrange how the elements will appear on the page.
+    icon.appendChild(dropDownIcon)
+    textContainer.appendChild(img)
+    textContainer.appendChild(title)
+    textContainer.appendChild(icon)
+    // Place the button on the page depending on which,
+    // page the user is on.
+    if (buttonClicked.includes('to-swap')) {
+      swapTokenButton.appendChild(textContainer)
+      // Make the input in the 'to section' larger as we make the button smaller.
+      toSwapInput.className = 'col-md-7 to-swap-input'
+    } else if (buttonClicked.includes('to-purchase')) {
+      purchaseTokenButton.appendChild(textContainer)
+      // Make the input in the 'to section' larger as we make the button smaller.
+      toPurchaseInput.className = 'col-md-7 to-input'
+    }
+  }
+
+}
+
 // Set the chosen token to the page.
 const setTokens = (evt) => {
     let swapFromImg = document.querySelector('.from-swap-image')
@@ -57,8 +126,12 @@ const setTokens = (evt) => {
           // Find the token with the same title as the selected title,
           // then display its image.
           if(token.title === selectedTitle) {
-            swapFromImg.src = token.imageUrl
-            swapFromTitle.innerHTML = selectedTitle
+            if (buttonClicked.includes('to-swap')) {
+              setSecondTokenButton(token.imageUrl, token.title, 'to-swap-image', 'to-swap-title')
+            } else {
+              swapFromImg.src = token.imageUrl
+              swapFromTitle.innerHTML = selectedTitle
+            }
           }
         })
       }
@@ -67,9 +140,15 @@ const setTokens = (evt) => {
         tokenData.map(token => {
           // Find the token with the same title as the selected title,
           // then display its image.
-          if(token.title === selectedTitle) {
+          if (token.title === selectedTitle) {
+          // Select whether to modify the button in the 'from section' or,
+          // 'to section'.
+          if (buttonClicked.includes('to-purchase')) {
+            setSecondTokenButton(token.imageUrl, token.title, 'to-purchase-image', 'to-purchase-title')
+          } else {
             purchaseFromImg.src = token.imageUrl
             purchaseFromTitle.innerHTML = selectedTitle
+          }
           }
         })
       }
